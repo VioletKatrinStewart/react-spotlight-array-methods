2886254a6f5c875805611946b40127cb3a1ec69a
Violet Stewart <vkazoo2@gmail.com>
2021-12-09 12:18:25 -0800

    worked on all problems

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 // INPUT: the array of countries from data.js
+import { brands, countries } from './data';
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  return countries.map((country) => country.name);
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  return countries.map((country) => [country.name, country.iso2]);
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries in Oceana
 // REQS: use .filter
 
-export const filterOceania = (countries) => {};
+export const filterOceania = (countries) => {
+  return countries.filter((country) => country.continent === 'Oceania');
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries that include the letters 'au'
 // REQS: use .filter
 
-export const filterAu = (countries) => {};
+export const filterAu = (countries) => {
+  return countries.filter((c) => c.name.includes('au'));
+};
 
 // INPUT: the array of brands from data.js
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
